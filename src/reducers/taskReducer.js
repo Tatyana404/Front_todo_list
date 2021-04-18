76b1777987643a9ffc3ac860ec3bb8775ec8ac0f
Tatyana404 <kot.kate2020@gmail.com>
2021-04-18 21:58:19 +0300

    Add front todo_list

---
+++ src/reducers/taskReducer.js
@@ -0,0 +1,106 @@
+import produce from 'immer';
+import ACTION_TYPES from 'actions/types';
+
+
+const initialState = {
+  tasks: [],
+  isFetching: false,
+  error: null,
+};
+
+const handlers = {
+  //create task
+  [ACTION_TYPES.CREATE_TASK_REQUEST]: produce((draftState, action) => {
+    draftState.isFetching = true;
+  }),
+
+  [ACTION_TYPES.CREATE_TASK_SUCCESS]: produce((draftState, action) => {
+    const {
+      payload: { task },
+    } = action;
+    draftState.tasks.push(task);
+  }),
+
+  [ACTION_TYPES.CREATE_TASK_ERROR]: produce((draftState, action) => {
+    const {
+      payload: { error },
+    } = action;
+    draftState.error = error;
+  }),
+
+  //update task
+  [ACTION_TYPES.UPDATE_TASK_REQUEST]: produce((draftState, action) => {
+    draftState.isFetching = true;
+  }),
+
+  [ACTION_TYPES.UPDATE_TASK_SUCCESS]: produce((draftState, action) => {
+    const {
+      payload: { tasks },
+    } = action;
+    const id = draftState.tasks.filter(task => task.id === tasks.id);
+    draftState.tasks[id] = tasks;
+  }),
+
+  [ACTION_TYPES.DELETE_TASK_ERROR]: produce((draftState, action) => {
+    const {
+      payload: { error },
+    } = action;
+    draftState.error = error;
+  }),
+
+  //get tasks
+  [ACTION_TYPES.GET_TASKS_REQUEST]: produce((draftState, action) => {
+    draftState.isFetching = true;
+  }),
+
+  [ACTION_TYPES.GET_TASKS_SUCCESS]: produce((draftState, action) => {
+    const {
+      payload: { tasks },
+    } = action;
+    draftState.tasks.push(...tasks);
+  }),
+
+  [ACTION_TYPES.GET_TASKS_ERROR]: produce((draftState, action) => {
+    const {
+      payload: { error },
+    } = action;
+    draftState.error = error;
+  }),
+
+  //delete task
+  [ACTION_TYPES.DELETE_TASK_REQUEST]: produce((draftState, action) => {
+    draftState.isFetching = true;
+  }),
+
+  [ACTION_TYPES.DELETE_TASK_SUCCESS]: produce((draftState, action) => {
+    const {
+      payload: { id },
+    } = action;
+    draftState.isFetching = false;
+    draftState.tasks = draftState.tasks.filter(t => t.id !== Number(id));
+  }),
+
+  [ACTION_TYPES.UPDATE_TASK_ERROR]: produce((draftState, action) => {
+    const {
+      payload: { error },
+    } = action;
+    draftState.error = error;
+  }),
+
+  //clear error
+  [ACTION_TYPES.CLEAR_TASK_ERROR]: produce((draftState, action) => {
+    draftState.error = null;
+  }),
+};
+
+function taskReducer (state = initialState, action) {
+  const { type } = action;
+  const handler = handlers[type];
+
+  if (handler) {
+    return handler(state, action);
+  }
+  return state;
+}
+
+export default taskReducer;
---
+++ src/sagas/taskSagas.js
@@ -0,0 +1,60 @@
+import { put } from 'redux-saga/effects';
+import * as ActionCreators from 'actions';
+import * as API from 'api';
+
+export function * createTaskSaga (action) {
+  try {
+    const { payload } = action;
+
+    const {
+      data: { data: task },
+    } = yield API.createTask({ ...payload.task });
+
+    yield put(ActionCreators.createTaskSuccess(task));
+  } catch (error) {
+    yield put(ActionCreators.createTaskError(error));
+  }
+}
+
+export function * updateTaskSaga (action) {
+  try {
+    const {
+      payload: { id },
+    } = action;
+
+    const {
+      data: { data: taskId },
+    } = yield API.updateTask({ taskId: id });
+
+    yield put(ActionCreators.updateTaskSuccess(taskId));
+  } catch (error) {
+    yield put(ActionCreators.updateTaskError(error));
+  }
+}
+
+export function * getTasksSaga (action) {
+  try {
+    const {
+      data: { data: tasks },
+    } = yield API.getTasks(action.payload);
+    yield put(ActionCreators.getTasksSuccess(tasks));
+  } catch (error) {
+    yield put(ActionCreators.getTasksError(error));
+  }
+}
+
+export function * deleteTaskSaga (action) {
+  try {
+    const {
+      payload: { id },
+    } = action;
+
+    const {
+      data: { data: taskId },
+    } = yield API.deleteTask({ taskId: id });
+
+    yield put(ActionCreators.deleteTaskSuccess(taskId));
+  } catch (error) {
+    yield put(ActionCreators.deleteTaskError(error));
+  }
+}
